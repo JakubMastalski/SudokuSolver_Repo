bf7fe894df74ba97384e150eee8f555cd241fd54
Jakub Mastalski <25287@student.ignatianum.edu.pl>
2024-09-27 14:12:41 +0200

    Divide Checking method

--- sudokuSolver_SOLID/sudokuSolver_SOLID/ButtonStart.h
+++ sudokuSolver_SOLID/sudokuSolver_SOLID/ButtonStart.h
@@ -9,6 +9,12 @@ ref class ButtonStart :
 public:
     void StartButton_Click(array<SudokuField^, 2>^ fieldsSudoku, Panel^ MainPanel);
     bool IsValid(array<SudokuField^, 2>^ fieldsSudoku, int row, int col, int value);
+    void RemoveInvalidNumbers(array<SudokuField^, 2>^ fieldsSudoku);
     void AddNumbersToBoard(array<SudokuField^, 2>^ fieldsSudoku);
+public:
+private:
+    void RemoveInvalidNumbersFromRows(array<SudokuField^, 2>^ fieldsSudoku);
+    void RemoveInvalidNumbersFromColumns(array<SudokuField^, 2>^ fieldsSudoku);
+    void RemoveInvalidNumbersFromBoxes(array<SudokuField^, 2>^ fieldsSudoku);
 };
 
