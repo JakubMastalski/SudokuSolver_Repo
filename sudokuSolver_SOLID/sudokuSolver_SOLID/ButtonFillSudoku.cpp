ec66125703375494c1cb211dc232ff1e387a8f71
Jakub Mastalski <25287@student.ignatianum.edu.pl>
2024-10-02 10:19:01 +0200

    Comit after merging

--- sudokuSolver_SOLID/sudokuSolver_SOLID/ButtonFillSudoku.cpp
+++ sudokuSolver_SOLID/sudokuSolver_SOLID/ButtonFillSudoku.cpp
@@ -1,7 +1,91 @@
 #include "ButtonFillSudoku.h"
 
 
-void ButtonFillSudoku::FillSudokuButton_Click()
+    sudokuFieldsGlobal = fieldsSudoku;
+    globalFillTimer = fillTimer;
+
+    currentRow = 0;
+    currentCol = 0;
+
+    fillTimer->Interval = 1;
+    fillTimer->Tick += gcnew EventHandler(this, &ButtonFillSudoku::OnTick);
+
+    fillTimer->Start();
+}
+
+void ButtonFillSudoku::OnTick(Object^ sender, EventArgs^ e)
 {
-	;
+    if (FillSudokuStep(sudokuFieldsGlobal, currentRow, currentCol)) 
+    {
+        globalFillTimer->Stop();
+        UpdateUI();
+        this->Visible = true;
+        this->Enabled = true;
+    }
+}
+
+bool ButtonFillSudoku::FillSudokuStep(array<SudokuField^, 2>^ fieldsSudoku, int row, int col)
+{
+    int emptyRow = -1, emptyCol = -1;
+
+    if (!FindEmptyLocation(emptyRow, emptyCol))
+        return true; 
+
+    for (int num = 1; num <= 9; num++)
+    {
+        if (num == 2)MessageBox::Show("EE");
+        if (IsValid(emptyRow, emptyCol, num))
+        {
+            fieldsSudoku[emptyRow, emptyCol]->SetValue(num, fieldsSudoku, emptyRow, emptyCol);
+            row = emptyRow;
+            col = emptyCol;
+            return false;
+        }
+    }
+
+    return false;
+}
+
+bool ButtonFillSudoku::FindEmptyLocation(int& row, int& col)
+{
+    for (row = 0; row < 9; row++)
+    {
+        for (col = 0; col < 9; col++)
+        {
+            if (sudokuFieldsGlobal[row, col]->GetValue() == 0 || sudokuFieldsGlobal[row, col]->Text == "")
+                return true;
+        }
+    }
+    return false;
+}
+
+bool ButtonFillSudoku::IsValid(int& row, int& col, int& value)
+{
+    for (int i = 0; i < 9; i++)
+    {
+        if (sudokuFieldsGlobal[row, i]->GetValue() == value || sudokuFieldsGlobal[i, col]->GetValue() == value)
+            return false;
+    }
+
+    int startRow = row / 3 * 3;
+    int startCol = col / 3 * 3;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (sudokuFieldsGlobal[startRow + i, startCol + j]->GetValue() == value)
+                return false;
+        }
+    }
+
+    return true;
+}
+
+void ButtonFillSudoku::UpdateUI()
+{
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            sudokuFieldsGlobal[i, j]->SetValue(sudokuFieldsGlobal[i, j]->GetValue(), sudokuFieldsGlobal, i, j);
+        }
+    }
 }
--- sudokuSolver_SOLID/sudokuSolver_SOLID/ButtonFillSudoku.h
+++ sudokuSolver_SOLID/sudokuSolver_SOLID/ButtonFillSudoku.h
@@ -4,6 +4,12 @@
 ref class ButtonFillSudoku :
     public IPanelButtons
 {
+private:
+    array<SudokuField^, 2>^ sudokuFieldsGlobal;  // Globalne pola Sudoku
+    System::Windows::Forms::Timer^ globalFillTimer;  // Globalny timer
+    int currentRow = -1;  // Aktualny wiersz
+    int currentCol = -1;  // Aktualna kolumna
+
 public:
     void FillSudokuButton_Click();
 };
